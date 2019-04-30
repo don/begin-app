ee26ebfd237a9c15fd0f52a095f28dbde1181f21
Don Coleman <dcoleman@chariotsolutions.com>
2019-04-30 00:16:05 -0400

    lint

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -5,35 +5,32 @@ let data = require('@begin/data')
 // let begin = require('@architect/functions')
 
 // TODO: modify the body object!
-let body = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon">
-  </head>
-  <body>
+// let body = `
+// <!doctype html>
+// <html lang=en>
+//   <head>
+//     <meta charset=utf-8>
+//     <title>Hi!</title>
+//     <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
+//     <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon">
+//   </head>
+//   <body>
 
-    <h1 class="center-text">
-      <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
-      Hello world!
-    </h1>
+//     <h1 class="center-text">
+//       <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
+//       Hello world!
+//     </h1>
 
-    <p class="center-text">
-      Your <a href="https://begin.com" class="link" target="_blank">Begin</a> app is ready to go!
-    </p>
+//     <p class="center-text">
+//       Your <a href="https://begin.com" class="link" target="_blank">Begin</a> app is ready to go!
+//     </p>
 
-  </body>
-</html>
-`
+//   </body>
+// </html>
+// `
 
 exports.handler = async function http(req) {
   console.log(req)
-
-  // shut the linter up
-  body += '';
 
   let table = await data.get({table:'foo'})
 
